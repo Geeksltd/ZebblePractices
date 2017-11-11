c25c0385c4512bb346b0b375f4b37dc1fd345c78
farzad <pharzath@gmail.com>
2017-11-11 21:26:45 +0330

    Add some inial files

---
+++ SimpleCalculator/SimpleCalculator/Run/UWP/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UWP
+{
+	class Program
+	{
+		public static void Main()
+		{
+			Zebble.UIRuntime.GetEntryAssembly = () => typeof(Program).GetAssembly();
+
+			Windows.UI.Xaml.Application.Start((p) => new App());
+		}
+	}
+}
